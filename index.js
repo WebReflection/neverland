3db013c65941421ed7fd5ab4005e4ac96c13a8d3
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2019-11-23 13:01:30 +0100

    updated domtagger to latest

--- index.js
+++ index.js
@@ -1161,6 +1161,17 @@ var neverland = (function (exports) {
     return String(this).replace(/^\s+|\s+/g, '');
   };
 
+  /* istanbul ignore next */
+
+  var normalizeAttributes = UID_IE ? function (attributes, parts) {
+    var html = parts.join(' ');
+    return parts.slice.call(attributes, 0).sort(function (left, right) {
+      return html.indexOf(left.name) <= html.indexOf(right.name) ? -1 : 1;
+    });
+  } : function (attributes, parts) {
+    return parts.slice.call(attributes, 0);
+  };
+
   function find$1(node, path) {
     var length = path.length;
     var i = 0;
@@ -1234,7 +1245,7 @@ var neverland = (function (exports) {
     var cache = new Map$1();
     var attributes = node.attributes;
     var remove = [];
-    var array = remove.slice.call(attributes, 0);
+    var array = normalizeAttributes(attributes, parts);
     var length = array.length;
     var i = 0;
 
@@ -1250,13 +1261,7 @@ var neverland = (function (exports) {
         /* istanbul ignore else */
 
         if (!cache.has(name)) {
-          var realName = parts.shift().replace(direct ? /^(?:|[\S\s]*?\s)(\S+?)\s*=\s*('|")?$/ : // TODO: while working on yet another IE/Edge bug I've realized
-          //        the current not direct logic easily breaks there
-          //        because the `name` might not be the real needed one.
-          //        Use a better RegExp to find last attribute instead
-          //        of trusting `name` is what we are looking for.
-          //        Thanks IE/Edge, I hate you both.
-          new RegExp('^(?:|[\\S\\s]*?\\s)(' + name + ')\\s*=\\s*(\'|")[\\S\\s]*', 'i'), '$1');
+          var realName = parts.shift().replace(direct ? /^(?:|[\S\s]*?\s)(\S+?)\s*=\s*('|")?$/ : new RegExp('^(?:|[\\S\\s]*?\\s)(' + name + ')\\s*=\\s*(\'|")[\\S\\s]*', 'i'), '$1');
           var value = attributes[realName] || // the following ignore is covered by browsers
           // while basicHTML is already case-sensitive
 
