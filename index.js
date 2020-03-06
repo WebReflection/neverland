04891e1f23fa01683cf128582c2ea5f50ec05051
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-03-06 11:44:28 +0100

    updated dependencies

--- index.js
+++ index.js
@@ -1615,10 +1615,11 @@ var neverland = (function (exports) {
     var type = name.slice(2);
     if (name.toLowerCase() in node) type = type.toLowerCase();
     return function (newValue) {
-      if (oldValue !== newValue) {
-        if (oldValue) node.removeEventListener(type, oldValue, false);
-        oldValue = newValue;
-        if (newValue) node.addEventListener(type, newValue, false);
+      var info = isArray(newValue) ? newValue : [newValue, false];
+
+      if (oldValue !== info[0]) {
+        if (oldValue) node.removeEventListener(type, oldValue, info[1]);
+        if (oldValue = info[0]) node.addEventListener(type, oldValue, info[1]);
       }
     };
   }; // special attributes helpers
@@ -1850,7 +1851,7 @@ var neverland = (function (exports) {
       render: function render(where, what) {
         var hole = typeof what === 'function' ? what() : what;
         var info = cache.get(where) || setCache(where);
-        var wire = hole instanceof Hole ? retrieve(Tagger, info, hole) : hole;
+        var wire = hole instanceof LighterHole ? retrieve(Tagger, info, hole) : hole;
 
         if (wire !== info.wire) {
           info.wire = wire;
@@ -1898,7 +1899,7 @@ var neverland = (function (exports) {
     return hole;
 
     function hole() {
-      return new Hole(type, tta.apply(null, arguments));
+      return new LighterHole(type, tta.apply(null, arguments));
     }
   };
 
@@ -1959,11 +1960,11 @@ var neverland = (function (exports) {
       var hole = args[i];
 
       if (typeof(hole) === 'object' && hole) {
-        if (hole instanceof Hole) args[i] = unroll(Tagger, info, hole, counter);else if (isArray(hole)) {
+        if (hole instanceof LighterHole) args[i] = unroll(Tagger, info, hole, counter);else if (isArray(hole)) {
           for (var _i = 0, _length = hole.length; _i < _length; _i++) {
             var inner = hole[_i];
 
-            if (typeof(inner) === 'object' && inner && inner instanceof Hole) {
+            if (typeof(inner) === 'object' && inner && inner instanceof LighterHole) {
               var sub = info.sub;
               var a = counter.a,
                   aLength = counter.aLength;
@@ -1983,11 +1984,13 @@ var neverland = (function (exports) {
     return length === 1 ? childNodes[0] : length ? new Wire(childNodes) : node;
   };
 
-  freeze(Hole);
-  function Hole(type, args) {
+  freeze(LighterHole);
+
+  function LighterHole(type, args) {
     this.type = type;
     this.args = args;
   }
+  var Hole = LighterHole;
 
   var _createRender = createRender(Tagger),
       render = _createRender.render,
